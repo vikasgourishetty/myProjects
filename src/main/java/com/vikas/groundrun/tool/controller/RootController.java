6131114f156071ff45d86e28cf8e49bd1f6dbe10
vikasgourishetty <vikasgourishetty@gmail.com>
2016-09-26 23:36:08 -0400

    Add Bootstrap, jsp & jstl dependancies

--- src/main/java/com/vikas/groundrun/tool/controller/RootController.java
+++ src/main/java/com/vikas/groundrun/tool/controller/RootController.java
@@ -1,14 +1,16 @@
 package com.vikas.groundrun.tool.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RootController {
 	
-	@RequestMapping("/")
+	// Can write to response body or can forward a view
+	/*@RequestMapping("/")
 	public String home() {
-		return "Hello World";
-	}
+		return "home";
+	}*/
 
 }
