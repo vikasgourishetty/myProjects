e639105242504fa800b404f80f6d7bab40edf87b
vikasgourishetty <vikasgourishetty@gmail.com>
2016-09-29 16:31:03 -0400

    jsr-303 validations
    Added JSR-303 spec validations.

--- src/main/java/com/vikas/groundrun/tool/dto/SignUpForm.java
+++ src/main/java/com/vikas/groundrun/tool/dto/SignUpForm.java
@@ -1,8 +1,24 @@
 package com.vikas.groundrun.tool.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+
 public class SignUpForm {
+	@NotNull
+	@Size(min=1, max=255)
+	//@Pattern(regexp="[a-zA-Z0-9._%-+]+@([a-zA-Z0-9-.]+\\.([a-zA-Z]{2,5}")
+	@Email
 	String email;
+	
+	@NotNull
+	@Size(min=1, max=100)
 	String name;
+	
+	@NotNull
+	@Size(min=6, max=15)
 	String password;
 	
 	public String getEmail() {
